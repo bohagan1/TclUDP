98f0b76adc8c9f06254b368b18df0321c85eeaa4
Brian O'Hagan <brian199@comcast.net>
2024-03-11 03:49:25 +0000

    Added TCL 9.0 build-info command including support for UUID files. Altered scripts from sample extension to put *Uuid.h in WIN_DIR not TMP_DIR.

--- generic/udp_tcl.c
+++ generic/udp_tcl.c
@@ -23,6 +23,7 @@
 #include <sys/types.h>
 #include "tcl.h"
 #include "udp_tcl.h"
+#include "udpUuid.h"
 
 #ifdef _WIN32
 #include <malloc.h>
@@ -1941,18 +1942,112 @@ int Udp_CmdProc(ClientData clientData, Tcl_Interp *interp, int objc, Tcl_Obj *co
     return TCL_ERROR;
 }
 
+/*
+ *----------------------------------------------------------------------
+ *
+ * Build Info Command --
+ *
+ *	Create command to return build info for package.
+ *
+ * Results:
+ *	A standard Tcl result
+ *
+ * Side effects:
+ *	Created build-info command.
+ *
+ *----------------------------------------------------------------------
+ */
+ 
+#ifndef STRINGIFY
+#  define STRINGIFY(x) STRINGIFY1(x)
+#  define STRINGIFY1(x) #x
+#endif
+ 
+int
+BuildInfoCommand(Tcl_Interp* interp) {
+    Tcl_CmdInfo info;
+
+    if (Tcl_GetCommandInfo(interp, "::tcl::build-info", &info)) {
+	Tcl_CreateObjCommand(interp, "::udp::build-info", info.objProc, (void *)(
+		PACKAGE_VERSION "+" STRINGIFY(UDP_VERSION_UUID)
+#if defined(__clang__) && defined(__clang_major__)
+			    ".clang-" STRINGIFY(__clang_major__)
+#if __clang_minor__ < 10
+			    "0"
+#endif
+			    STRINGIFY(__clang_minor__)
+#endif
+#if defined(__cplusplus) && !defined(__OBJC__)
+			    ".cplusplus"
+#endif
+#ifndef NDEBUG
+			    ".debug"
+#endif
+#if !defined(__clang__) && !defined(__INTEL_COMPILER) && defined(__GNUC__)
+			    ".gcc-" STRINGIFY(__GNUC__)
+#if __GNUC_MINOR__ < 10
+			    "0"
+#endif
+			    STRINGIFY(__GNUC_MINOR__)
+#endif
+#ifdef __INTEL_COMPILER
+			    ".icc-" STRINGIFY(__INTEL_COMPILER)
+#endif
+#ifdef TCL_MEM_DEBUG
+			    ".memdebug"
+#endif
+#if defined(_MSC_VER)
+			    ".msvc-" STRINGIFY(_MSC_VER)
+#endif
+#ifdef USE_NMAKE
+			    ".nmake"
+#endif
+#ifndef TCL_CFG_OPTIMIZED
+			    ".no-optimize"
+#endif
+#ifdef __OBJC__
+			    ".objective-c"
+#if defined(__cplusplus)
+			    "plusplus"
+#endif
+#endif
+#ifdef TCL_CFG_PROFILED
+			    ".profile"
+#endif
+#ifdef PURIFY
+			    ".purify"
+#endif
+#ifdef STATIC_BUILD
+			    ".static"
+#endif
+		), NULL);
+    }
+    return TCL_OK;
+}
+/*
+ *-----------------------------------------------------------------------------
+ *
+ * udpInit --
+ *
+ *	This procedure is the main initialization point of the UDP
+ *	extension.
+ *
+ * Results:
+ *	Returns a standard Tcl completion code, and leaves an error
+ *	message in the interp's result if an error occurs.
+ *
+ * Side effects:
+ *	Adds a command to the Tcl interpreter.
+ *
+ *-----------------------------------------------------------------------------
+ */
 
 #if TCL_MAJOR_VERSION > 8
 #define MIN_VERSION "9.0"
 #else
-#define MIN_VERSION "8.4"
+#define MIN_VERSION "8.5"
 #endif
 
-/*
- * ----------------------------------------------------------------------
- * udpInit
- * ----------------------------------------------------------------------
- */
 int Udp_Init(Tcl_Interp *interp) {
 #if defined(DEBUG) && !defined(_WIN32)
     dbg = fopen("udp.dbg", "wt");
@@ -1979,6 +2074,8 @@ int Udp_Init(Tcl_Interp *interp) {
     Tcl_CreateObjCommand(interp, "udp_conf", udpConf, (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
     Tcl_CreateObjCommand(interp, "udp_peek", udpPeek, (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
     Tcl_CreateObjCommand(interp, "udp", Udp_CmdProc, (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
+
+    BuildInfoCommand(interp);
 
     return Tcl_PkgProvide(interp, PACKAGE_NAME, PACKAGE_VERSION);
 }
