dc7ab8376dcf159635542c1f431bf6694409b6a0
Brian O'Hagan <brian199@comcast.net>
2024-07-06 19:13:38 +0000

    Changed to use Tcl_PkgRequireEx, only call if not using stubs.

--- generic/udp_tcl.c
+++ generic/udp_tcl.c
@@ -2559,10 +2559,11 @@ int Udp_Init(Tcl_Interp *interp) {
     if (Tcl_InitStubs(interp, MIN_VERSION, 0) == NULL) {
 	return TCL_ERROR;
     }
-#endif
-    if (Tcl_PkgRequire(interp, "Tcl", MIN_VERSION, 0) == NULL) {
+#else
+    if (Tcl_PkgRequireEx(interp, "Tcl", MIN_VERSION, 0, NULL) == NULL) {
 	return TCL_ERROR;
     }
+#endif
 
 #ifdef _WIN32
     if (Udp_WinHasSockets(interp) != TCL_OK) {
